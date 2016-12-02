eaeccd817c5ba7645721b788baa06a97286a28e6
jgararuna <jgararuna@hotmail.com>
2016-12-02 19:07:42 -0200

    adjusting calc_utils.py

--- calc_utils.py
+++ calc_utils.py
@@ -3,18 +3,18 @@ import math
 DISTANCE_CONST1 = 0.00000125
 DISTANCE_CONST2 = 0.00000158
 DISTANCE_CONST3 = 0.000000125
-ARM = 0.31
+ARM = 0.75
 def calc_distance1(prx):
-    return DISTANCE_CONST1/prx
+    return (-0.003811*(math.pow(prx, 3))) + (0.4172*(math.pow(prx, 2))) - (14.78*prx) + 171.9
 
 def calc_distance2(prx):
-    return DISTANCE_CONST2/prx
+    return -0.002781*(prx**3) + 0.3536*(prx**2) - 14.45*prx + 191.7
 
 def calc_distance3(prx):
-    return DISTANCE_CONST3/prx
+    return -0.003137*(prx**3) + 0.3713*(prx**2) - 14.11*prx + 175.4
 
 def dbm_to_potency(dbm):
-    return 0.001*math.pow(10, float(float(dbm)/10))
+    return -1*dbm
 
 def calc_distance_from_dbm1(dbm):
     return calc_distance1(dbm_to_potency(dbm))
@@ -26,8 +26,17 @@ def calc_distance_from_dbm3(dbm):
     return calc_distance3(dbm_to_potency(dbm))
 
 def get_x(d1, d2):
-    return float((d1 - d2)/(4*ARM))
+    return float(math.pow(d1, 2) - math.pow(d2, 2))/(4*ARM)
 
 def get_y(d1, d2 ,d3):
-    return float((1/(2*ARM))*(d3 -(d2/2)-(d1/2)))
-    
+    return float(1/(2*ARM))*((math.pow(d3, 2)) - (math.pow(d2, 2)/2)-(math.pow(d1, 2)/2))
+
+def main():
+        print calc_distance1(37)
+        print calc_distance2(40)
+        print calc_distance3(43)
+
+        print get_x(calc_distance1(42), calc_distance2(40))
+        print get_y(calc_distance1(42), calc_distance2(40), calc_distance3(35))
+        
+if __name__ == "__main__": main()    
