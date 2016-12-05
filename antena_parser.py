f553e86b9f9236641167301434c2a0e41d5e8428
Vinicius Franco <viniciusf.arantes@gmail.com>
2016-12-05 12:14:00 +0000

    medicoes

--- antena_parser.py
+++ antena_parser.py
@@ -42,8 +42,8 @@ def parse_antena_1():
     """
     i = 10
     for x in range(0, 11):
-        with open("antena_1_medicoes/antena_1_" + str(i) + "m.txt", "r+") as f:
-            fo = open("antena_1_medicoes/antena_1_" + str(i) + "_parsed.txt", "wb") 
+        with open("antena1/antena_1_" + str(i) + "m.txt", "r+") as f:
+            fo = open("antena1/antena_1_" + str(i) + "_parsed.txt", "wb") 
             for line in f:
                 if 'antena_1' in line:
                     fo.write(str(get_num_antena_1(line)))
@@ -60,8 +60,8 @@ def parse_antena_2():
     """
     i = 10
     for x in range(0, 11):
-        with open("antena_2_medicoes/antena_2_" + str(i) + "m.txt", "r+") as f:
-            fo = open("antena_2_medicoes/antena_2_" + str(i) + "_parsed.txt", "wb") 
+        with open("antena2/antena_2_" + str(i) + "m.txt", "r+") as f:
+            fo = open("antena2/antena_2_" + str(i) + "_parsed.txt", "wb") 
             for line in f:
                 if 'antena_2' in line:
                     fo.write(str(get_num_antena_2(line)))
@@ -77,8 +77,8 @@ def parse_antena_3():
     """
     i = 10
     for x in range(0, 11):
-        with open("antena_3_medicoes/antena_3_" + str(i) + "m.txt", "r+") as f:
-            fo = open("antena_3_medicoes/antena_3_" + str(i) + "_parsed.txt", "wb") 
+        with open("antena3/antena_3_" + str(i) + "m.txt", "r+") as f:
+            fo = open("antena3/antena_3_" + str(i) + "_parsed.txt", "wb") 
             for line in f:
                 if 'antena_3' in line:
                     fo.write(str(get_num_antena_3(line)))
